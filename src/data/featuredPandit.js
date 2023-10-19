e253cabdb23176ad65a7af198c9b0c1f8298ecb8
The Captaan <118829565+thecaptaan@users.noreply.github.com>
2023-10-19 12:16:51 +0530

    Add files via upload

--- src/data/featuredPandit.js
+++ src/data/featuredPandit.js
@@ -2,16 +2,16 @@ module.exports = [
     {
         panditId: 1,
         name: 'Hari Sankar',
-        image: "profile1.jpeg",
+        image: "https://elasticbeanstalk-ap-south-1-679122753279.s3.ap-south-1.amazonaws.com/realpandit/images/profile1.jpeg",
     },
     {
         panditId: 2,
         name: 'Rajesh Sharma',
-        image: "profile2.jpeg",
+        image: "https://elasticbeanstalk-ap-south-1-679122753279.s3.ap-south-1.amazonaws.com/realpandit/images/profile2.jpeg",
     },
     {
         panditId: 3,
         name: 'Himanshu Tiwari',
-        image: "profile3.jpeg",
+        image: "https://elasticbeanstalk-ap-south-1-679122753279.s3.ap-south-1.amazonaws.com/realpandit/images/profile3.jpeg",
     }
 ]
