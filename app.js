5516edd0c0cb167c80bca4bf3cad7b8d06d6e2bf
The Captaan <118829565+thecaptaan@users.noreply.github.com>
2023-10-19 06:56:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict"
+require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('node:path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
+const server = express();
+
+server.use(cors())
+server.use(helmet())
+server.use(cookieParser())
+server.use(express.json())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
+
+server.use(expressLayouts)
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'src/views'))
+
+const PORT = process.env.PORT || 8080
+
+server.use(express.static(path.join(__dirname, 'src/assets/fonts')))
+server.use(express.static(path.join(__dirname, 'src/assets/videos')))
+server.use(express.static(path.join(__dirname, 'src/assets/scripts')))
+server.use(express.static(path.join(__dirname, 'src/assets/styles')))
+server.use(express.static(path.join(__dirname, 'src/assets/images')))
+
+const unAuth = require('./src/routers/unAuth')
+
+server.use(unAuth)
+server.use((req, res, next) => {
+    res.status(404).render('error/404')
+})
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
